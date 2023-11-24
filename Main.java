a43957cce41f69944a53663b1793584cadb6d460
Emília Querino <emiliaquerino@gmail.com>
2023-11-24 15:01:37 -0300

    Cria menu do programa

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("carrinho de compras");
+        CartManager cartManager = new CartManager();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Adicionar produto\n2. Remover produto\n3. Exibir carrinho\n4. Sair");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    cartManager.addProduct();
+                    break;
+                case 2:
+                    cartManager.removeProduct();
+                    break;
+                case 3:
+                    System.out.println("carrinho de compras"); //remover essa linha posteriori 
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
     }
 }
