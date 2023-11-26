791c8380fb12d1ee7637ae614ab004cfb19b9aab
Scalu-Neo <codigomatrix10@gmail.com>
2023-11-26 08:50:55 -0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,11 +1,40 @@
 
 public class Product {
 
-    public Product(String name, double price) {
+    //Variáveis de instância
+    private String productName;
+    private double price;
+    private int productId;
+
+    //método construtor
+    public Product(String name, double price, int productId) {
+        this.name=name;
+        this.price=price;
+        this.productId=productId;
+    }
+    //métodos Get e Set
+    public String getProductName() {
+        return productName;
     }
 
-    public int getPrice() {
-        return 0;
+    public void setProductName(String productName){
+        this.productName=productName;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+    public int getProductId(){
+        return productId;
+    }
+
+    public void setProductId(int product){
+        this.productId=productId;
     }
 
 }
