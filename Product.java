d2c77172b458d32916919ddb10bea4d6c5d37cdf
Emília Querino <emiliaquerino@gmail.com>
2023-11-27 09:43:26 -0300

    Remove Id de product

--- Product.java
+++ Product.java
@@ -1,40 +1,29 @@
 
 public class Product {
 
-    //Variáveis de instância
     private String productName;
     private double price;
-    private int productId;
 
-    //método construtor
     public Product(String productName, double price) {
         this.productName = productName;
         this.price = price;
-        
+
     }
-    //métodos Get e Set
+
     public String getProductName() {
         return productName;
     }
 
-    public void setProductName(String productName){
-        this.productName=productName;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price){
-        this.price=price;
-    }
-
-    public int getProductId(){
-        return productId;
-    }
-
-    public void setProductId(int product){
-        this.productId=productId;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
 }
