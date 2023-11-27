0549b3c1e6dd4a942d99a2c66d9d425a8a6ad49d
Emília Querino <emiliaquerino@gmail.com>
2023-11-27 19:34:36 -0300

    Remove import java.util.Iterator

--- Main.java
+++ Main.java
@@ -4,27 +4,27 @@ public class Main {
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
         CartManager cartManager = new CartManager(cart);
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("1. Adicionar produto\n2. Remover produto\n3. Exibir carrinho\n4. Sair");
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    cartManager.addProduct();
-                    break;
-                case 2:
-                    cartManager.removeProduct();
-                    break;
-                case 3:
-                    cart.displayCart();
-                    break;
-                case 4:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-                    break;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("1. Adicionar produto\n2. Remover produto\n3. Exibir carrinho\n4. Sair");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        cartManager.addProduct();
+                        break;
+                    case 2:
+                        cartManager.removeProduct();
+                        break;
+                    case 3:
+                        cart.displayCart();
+                        break;
+                    case 4:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
             }
         }
     }
